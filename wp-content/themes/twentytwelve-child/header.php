0ce0e2979749fe84ee9b3b93f5174b6d55ccd48b
alejandro <alejandro.cavallo@gmail.com>
2015-05-22 18:30:25 -0300

    se agrega template y estilo para breadcrumbs

--- wp-content/themes/twentytwelve-child/header.php
+++ wp-content/themes/twentytwelve-child/header.php
@@ -54,3 +54,9 @@
 	
 
 	<div id="main" class="wrapper">
+            <div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+                <?php if(function_exists('bcn_display'))
+                {
+                    bcn_display();
+                }?>
+            </div>
